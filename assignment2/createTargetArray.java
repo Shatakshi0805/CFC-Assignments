205a421d1e13668ffb1c3386cb26f62214361d57
Shatakshi <shatakshi.gupta85@gmail.com>
2021-11-26 10:07:58 +0530

    add createTargetArray leetcode question assignment2
    Signed-off-by: Shatakshi <shatakshi.gupta85@gmail.com>

---
+++ assignment2/createTargetArray.java
@@ -0,0 +1,35 @@
+package assignment2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class createTargetArray {
+    public static void main(String[] args) {
+        int[] nums = {-7,-3,2,3,11};
+
+        int front = 0;
+        int mid = ((0)+nums.length-1)/2;
+        int end = nums.length-1;
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(end >= mid && front <= mid) {
+            if (Math.abs(nums[front]) <= Math.abs(nums[end])) {
+                list.add(nums[end]*nums[end]);
+                end--;
+            } else if (Math.abs(nums[front]) > Math.abs(nums[end])) {
+                list.add(nums[front] * nums[front]);
+                front++;
+            }
+        }
+//        list.add(end);
+        Collections.sort(list);
+
+        Integer[] arr = new Integer[list.size()];
+        arr = list.toArray(arr);
+
+        for (Integer x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
