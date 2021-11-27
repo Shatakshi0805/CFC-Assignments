1c1881b83c5fa59711a23c84da93205be1c38759
Shatakshi <shatakshi.gupta85@gmail.com>
2021-11-27 08:51:03 +0530

    modified while loop condition, added comments and solution  now works for every input
    Signed-off-by: Shatakshi <shatakshi.gupta85@gmail.com>

--- assignment2/createTargetArray.java
+++ assignment2/createTargetArray.java
@@ -8,13 +8,13 @@ public class createTargetArray {
         int[] nums = {-7,-3,2,3,11};
 
         int front = 0;
-        int mid = ((0)+nums.length-1)/2;
-        int end = nums.length-1;
+        int mid = ((0)+nums.length-1)/2;//mid is not used but it kind of helped to maintain some point while-- 
+        int end = nums.length-1;//--moving ahead and backwards
 
         ArrayList<Integer> list = new ArrayList<>();
 
-        while(end >= mid && front <= mid) {
-            if (Math.abs(nums[front]) <= Math.abs(nums[end])) {
+        while(end >= front) {//when end becomes less than front, that value has already been checked by front,--
+            if (Math.abs(nums[front]) <= Math.abs(nums[end])) {//-- cause that is why front is ahead of end
                 list.add(nums[end]*nums[end]);
                 end--;
             } else if (Math.abs(nums[front]) > Math.abs(nums[end])) {
@@ -22,7 +22,9 @@ public class createTargetArray {
                 front++;
             }
         }
-//        list.add(end);
+        if (front == end) {//there might be condn when front & end point to same value like {0,1,2,3}
+            list.add(nums[end]*nums[end]);//here eventually front & end point to same value that is zero index value
+        }
         Collections.sort(list);
 
         Integer[] arr = new Integer[list.size()];
